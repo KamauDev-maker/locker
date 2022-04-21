5f480e362d02c05130486a0bfc357b52531c5817
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-21 21:18:24 +0300

    creating method for finding username & checking if user exist

--- users.py
+++ users.py
@@ -98,3 +98,29 @@ class Users:
         method removes user details
         """  
         Users.users_list.remove(self) 
+        
+    @classmethod
+    def  find_by_username(cls,username):
+        """
+        authenticate user 
+
+        Args:
+            username:name used by user to login in
+        """
+        for user in Users.users_list:
+            if user.username == username:
+                return user
+            
+    @classmethod
+    def  user_exists(cls,username,login_password):
+        """
+        authenticate user password by checking if the user exist
+
+        Args:
+            username :username used to login
+            login_password:password for the user
+        """
+        for user in Users.user_list:
+            if user.username == username and user.login_password == login_password:
+                return True
+        return False   
