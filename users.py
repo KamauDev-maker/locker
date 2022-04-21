9c77ced14b6dda5456128538dbb8b24efc32f32e
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-21 20:04:00 +0300

    creating a method that takes in application name & retuns the said application

--- users.py
+++ users.py
@@ -22,3 +22,22 @@ class Credentials:
         """method that adds credentials into credentials_list
         """
         Credentials.credentials__list.append(self)
+    
+    def  delete_credentials(self):
+        '''
+        delete_credentials method that deletes credentials from credential_list
+        ''' 
+        Credentials.credentials__list.remove(self)
+        
+    @classmethod
+    def  find_by_application_name(cls,application_name):
+        """method  that takes in application name and returns the credentials for the said application
+
+        Args:
+            application_name: name of application to be found
+        """
+        for credential in Credentials.credentials__list:
+            if credential.application_name == application_name:
+                return credential
+            
+        
