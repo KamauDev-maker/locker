3852726a04b3656616a6fbd7d896090c076b3741
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-22 12:48:40 +0300

    creating conditions that should be met on username and login password

--- credentials.py
+++ credentials.py
@@ -81,5 +81,39 @@ def main():
     print("--"*16)
     print("An Application that manages your password. ")
     while True:
-        print("What is your name? ")    
-    
+        print("What is your name? ") 
+        current_user = input().strip(' ').capitalize()
+        if current_user != '':
+            print(f"\nHello {current_user},")
+            while True:
+                print("Kindly use these short Codes to navigate through the application :\n CA - create account \n LI - login into an existing account \n DEL - delete your acount \n EX - exit the application")
+                short_code = input().upper()
+                
+                if short_code == 'CA':
+                    print("\nCREATE AN ACCOUNT")
+                    print("-"*17)
+                    print("Choose your username")
+                    print(" "*6 +"*the username must contain alphabetical letters and no spaces*")
+                    
+                    while True:
+                        username = input().capitalize()
+                        if username.isalpha():
+                            print("Enter a password")
+                            print(" "*6 +"*the password must be 8 character or more characters*")
+                            while True:
+                                login_password = input()
+                                if len(login_password) >=8:
+                                    add_user(create_user(username,login_password))
+                                    print(f"\nAccount for {username} has been successfully created.  \nProceed to login. \n")
+                                    break
+                                else:
+                                    print("\nThe username entered is invalid. ")
+                                    print("Please use alphabetical letters and no spaces. ")
+                                    continue
+                        else:
+                             print("\nThe password you entered is too short.")
+                             print("Please use a password of 8 characters or more.")
+                             continue
+                        break           
+                                                
+            
